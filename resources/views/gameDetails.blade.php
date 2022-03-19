09dea51ba9ffa12ed776f8005583330dd3744864
TKF486 <60849963+TKF486@users.noreply.github.com>
2022-03-19 21:56:44 +0800

    game details
    -show out the output from databases individually

--- resources/views/gameDetails.blade.php
+++ resources/views/gameDetails.blade.php
@@ -4,11 +4,30 @@
 <p>gameName</p> 
 <p>{{$game['gameName']}}</p>
 
-<p>gameDesc</p> 
+<p>gamePrice</p> 
+<p>{{$game['gamePrice']}}</p>
+
+<p>gameDesc</p>
 <p>{{$game['gameDesc']}}</p>
 
-<p>gamgamePublishereName</p>
+<p>gamePublisher</p>
 <p>{{$game['gamePublisher']}}</p>
 
+<p>gameAgeRating</p>
+<p>{{$game['gameAgeRating']}}</p>
+
+<p>gameGenre</p>
+<p>{{$game['gameGenre']}}</p>
+
+<p>gameReleaseDate</p>
+<p>{{$game['gameReleaseDate']}}</p>
+
+<p>gameLanguage</p>
+<p>{{$game['gameLanguage']}}</p>
+
+<p>gameRequirement</p>
+<p>{{$game['gameRequirement']}}</p>
+
 @endforeach
-      
+
+<button>Checkout</button>
