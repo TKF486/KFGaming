4ab06d63f49b48e1f5dcd1b8ca5895a1fa580a83
Desmond325 <87235152+Desmond325@users.noreply.github.com>
2022-04-03 14:38:43 +0800

    gamelisting Test
    insert image for testing

--- resources/views/gameListing.blade.php
+++ resources/views/gameListing.blade.php
@@ -1,7 +1,102 @@
-<h1>Game Listing</h1>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<select name="games" id="games">
-    <option value="FPS">FPS</optional>
-    <option value="RPG">RPG</optional>  
-    <option value="Adventure">Adventure</optional>
-</select>
+<body>
+<div class="upper">
+<h1>Game Listing</h1>
+<div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">FPS</a>
+    <a href="#">RPG</a>
+    <a href="#">Adventure</a>
+  </div>
+</div>
+</div>
+
+<div class="store">
+<div>
+<img src="/img/apex.png" alt="apex">
+<img src="/img/csgo.jpg" alt="apex">
+<img src="/img/cyberpunk.jpg" alt="apex">
+<img src="/img/dota2.jpg" alt="apex">
+<img src="/img/dragonball.jpg" alt="apex">
+<img src="/img/dying-light-2_je11.300.jpg" alt="apex">
+<img src="/img/dying-light-cover.jpg" alt="apex">
+<img src="/img/ff6.png" alt="apex">
+<img src="/img/final-fantasy-15.jpg" alt="apex">
+<img src="/img/forza-horizon-4-cover.jpg" alt="apex">
+<img src="/img/forza-horizon-5-button-fin-1629830068379.jpg" alt="apex">
+<img src="/img/hitman.jpg" alt="apex">
+<img src="/img/little-nightmares-cover.jpg" alt="apex">
+<img src="/img/little-nightmares-ii-cover.jpg" alt="apex">
+<img src="/img/MHR.jpg" alt="apex">
+<img src="/img/MHW.jpg" alt="apex">
+<img src="/img/minecraft.jpg" alt="apex">
+<img src="/img/naruto.jpg" alt="apex">
+<img src="/img/rainbow6.jpg" alt="apex">
+<img src="/img/valorant.png" alt="apex">
+</div>
+</div>
+</body>
+
+
+<style>
+
+h1 {
+    color: #06FF00;
+    font-family: sans-serif;
+}
+.upper {
+    background-color: black;
+}
+body {
+    background-color: #444444;
+}
+.dropbtn {
+  background-color: #06FF00;
+  color: white;
+
+  padding: 16px;
+  font-size: 16px;
+  font-family: sans-serif;
+  border: none;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.store img {
+    margin: 10px;
+    margin-left: 55px;
+}
+
+.dropdown-content a:hover {background-color: #ddd;}
+
+.dropdown:hover .dropdown-content {display: block;}
+
+.dropdown:hover .dropbtn {background-color: #3e8e41;}
+
+
+</style>
+</head>
+
+
