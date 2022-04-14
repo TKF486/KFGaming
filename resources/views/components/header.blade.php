e01256c69b9f56b2e4ed1a5e6fb6b07fe7142727
Jermin <69464716+jmeyu@users.noreply.github.com>
2022-04-14 15:56:22 +0800

    Update header blade
    Update user profile at header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -29,7 +29,7 @@
     <!-- <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block"> -->
         @canany(['isUser', 'isAdmin'])
             <div class="topnav-right">
-                <a href="{{ url('/home') }}"
+                <a href="{{ url('/userProfile') }}"
                    class="text-sm text-gray-700 dark:text-gray-500 underline">Hello, {{session('name')}}</a>
                 <a href="{{ url('logout') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Logout</a>
             </div>
